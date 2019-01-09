b6f070838cd10a0dac3b88d81de12d3c9f87d069
Greg Morgan <gregm9999@gmail.com>
2019-01-09 05:24:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function driversByRevenue(drivers) {
 
 function driversByName(drivers) {
   return drivers.sort(function(a,b) {
-    return a.name > b.name;
+    return a.localeCompare(b)
   })
 }
 
