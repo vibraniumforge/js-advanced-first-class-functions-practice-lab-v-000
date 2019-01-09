97138c142b73ba5132c4c1f0f7dfcb2cb7d9a897
Greg Morgan <gregm9999@gmail.com>
2019-01-09 05:45:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function totalRevenue(drivers) {
   const reducer = function (currentValue, accumulator) {
     return currentValue + accumulator.revenue
   }
-  drivers.reduce(reducer)
+  drivers.reduce(reducer, 0)
 }
 
 function averageRevenue(drivers) {
