5251ef248c78442b43e13c4a4894a82615e40094
Greg Morgan <gregm9999@gmail.com>
2019-01-09 05:05:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function logDriverNames(drivers) {
 function logDriversByHometown(drivers, hometown) {
   drivers.forEach(function(driver) {
     if (driver.hometown===hometown) {
-      console.log(hometown)
+      console.log(driver.name)
     }
 
   })
